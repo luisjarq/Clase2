2fd762482b9e9d98a1921643131a0b4a135cc3f7
luisjarq <lc.luisja@gmail.com>
2022-04-25 16:30:55 +0200

    fix

---
+++ Clase 1/Program.cs
@@ -0,0 +1,26 @@
+﻿class Example
+{
+    public static void Main()
+    {for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"i vale: {i}");
+        }
+        int valorA = 0;
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.WriteLine($"i vale: {i}");
+        //}
+
+        //int[] array = { 0, 1, 2, 3 };
+        List<string> lista = new List<string>() { "gato", "perro" };
+
+        //foreach (string i in lista)
+        //{
+        //    Console.WriteLine($"i vale: {i}");
+        //}
+        for (int i = 0; i < lista.Count; i++)
+        {
+            Console.WriteLine($"i vale: {lista[i]}");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+string path = @"C:\Users\lclui\Desktop\ejemplo.txt";
+string[] arrayDeTexto = File.ReadAllLines(path);
+List<string> lineasDeTexto = new List<string>(arrayDeTexto);
+int contador = 0;
+foreach (string linea in lineasDeTexto)
+{
+    string[] palabras = linea.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    foreach (string palabra in palabras)
+    {
+        if (palabra == "Cervantes")
+        {
+            contador++;
+        }
+    }
+}
+Console.WriteLine(contador);
+
+
+
